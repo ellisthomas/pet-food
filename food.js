ca2639de56a8c9caf6681b76f9ddfdd1c5d279da
ellisthomas <ellisthomas@gmail.com>
2017-03-09 20:05:55 -0600

    dog info printing out to the DOM

--- food.js
+++ food.js
@@ -3,39 +3,41 @@ var dogContainer = document.getElementById("dogContainer");
 function makeDom(xhrData) {
 	var dogString = "" 
 	var currentFood; 
+	// console.log("currentFood", xhrData.dog_brands);
+	
 	for (var i = 0; i < xhrData.dog_brands.length; i++) {
 		currentFood = xhrData.dog_brands[i];
-
-		dogString += `<div class="col-sm-6 col-md-4">`;
-		dogString += `<div class="thumbnail">`;
+		
 		dogString += `<h3>${currentFood.name}</h3>`;
-		dogString += `</div></div>`;
-  console.log("currentFood", currentFood);
+
+		for (var j = 0; j < xhrData.dog_brands[i].types.length; j++) {
+	  
+			dogString += `<h3>${currentFood.types[j].type}</h3>`;
+			// console.log("my type", xhrData.dog_brands[i].types);
+			
+			for (var k = 0; k < xhrData.dog_brands[i].types[j].volumes.length; k++) {
+
+				dogString += `<h3>${currentFood.types[j].volumes[k].name}</h3>`;
+				dogString += `<h3>${currentFood.types[j].volumes[k].price}</h3>`;
+			}
+		}
+
 	}
 
-	for (var j = 0; j < xhrData.dog_brands.types; j++) {
-		var currentType = xhrData.dog_brands.types[i];
-		condole.log("my type", currentType);
-
-		dogString += `<div class="col-sm-6 col-md-4">`;
-		dogString += `<div class="thumbnail">`;
-		dogString += `<h3>${currentFood.type}</h3>`;
-		dogString += `</div></div></div>`;
-
-	
-	}
 		dogContainer.innerHTML = dogString;
 }
 
 function executeThisCodeAfterFileLoaded() {
-	console.log("Data Returned: ", data);
 	var data = JSON.parse(this.responseText);
-	makeDom(data);
+	makeDom(data);	
+	console.log(data);
 }
 
+
 function executeThisCodeAfterFileFails() {
-	console.log("boooooo");
 }
+
+
 var myRequest = new XMLHttpRequest();
 myRequest.addEventListener("load", executeThisCodeAfterFileLoaded);
 myRequest.addEventListener("error", executeThisCodeAfterFileFails);
